893db21f35c9c871beff6b6a1724e2411836b499
huangtao800 <huangtao800@gmail.com>
2014-03-22 21:40:20 +0800

    大体完成CommentService

--- comparePrice/src/edu/nju/comparePrice/services/CommentService.java
+++ comparePrice/src/edu/nju/comparePrice/services/CommentService.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 
 public class CommentService {
 
+	private static final int SENSITIVECOUNT=10;
+	
 	private static CommentService commentService;
+	private ArrayList<String> sensitiveWords;	//initialized by DAOFacase
+	private ArrayList<String> specialWords;		//initialized by DAOFacase
 
 	private CommentService() {
 
@@ -18,14 +22,38 @@ public class CommentService {
 	}
 
 	public void postComment(int userID, String comment) {
-
+		if(!checkComment(userID, comment)){
+			//addComment
+		}else{
+			//detect water navy
+		}
 	}
 
-	public void checkComment(int userID, String comment) {
-
+	public boolean checkComment(int userID, String comment) {
+		boolean result =false;
+		for(int i=0;i<sensitiveWords.size();i++){
+			if(comment.contains(sensitiveWords.get(i))){
+				//add sensitiveCount
+				//set sensitiveFlag
+				result=true;
+			}
+				
+		}
+		
+		for(int i=0;i<specialWords.size();i++){
+			if(comment.contains(specialWords.get(i))){
+				//set SpecialFlag
+				result=true;
+			}
+				
+			return true;
+		}
+		return result;
 	}
 
-	public void checkWaterNavy(int userID, ArrayList<String> commentList) {
-
+	public boolean checkWaterNavy(int userID) {
+		int sensitiveCount=0;
+		//获取用户的SensitiveCount
+		return sensitiveCount>=SENSITIVECOUNT;
 	}
 }
