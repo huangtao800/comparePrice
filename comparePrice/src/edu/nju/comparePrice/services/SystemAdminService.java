5513cf6e684ef58c044b17755363c4bcadc86aaf
董仁广 <dongrenguang@gmail.com>
2014-04-07 19:49:34 +0800

    aa

--- comparePrice/src/edu/nju/comparePrice/services/SystemAdminService.java
+++ comparePrice/src/edu/nju/comparePrice/services/SystemAdminService.java
@@ -2,16 +2,21 @@ package edu.nju.comparePrice.services;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.nju.comparePrice.dao.DaoFacade;
 import edu.nju.comparePrice.models.Brand;
 import edu.nju.comparePrice.models.Commodity;
 import edu.nju.comparePrice.models.SensitiveWord;
 
+
 public class SystemAdminService {
-	
+	@Autowired 
+	private DaoFacade daoFacade;	
 	
 	//获取已经被屏蔽的商品列表
 	public ArrayList<Commodity> getForbiddenCommodities(){
-		return null;
+		return daoFacade.getForbiddenCommodities();
 	}
 	
 	public ArrayList<Commodity> getToForbidCommodities(String commodityName){
