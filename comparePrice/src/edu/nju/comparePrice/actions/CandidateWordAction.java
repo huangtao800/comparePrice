f8d02f6fe71896859415858d9ca7f4243f01c758
Hazel <cfzljcl@gmail.com>
2014-04-11 21:06:39 +0800

    增加了候选词的功能

--- comparePrice/src/edu/nju/comparePrice/actions/CandidateWordAction.java
+++ comparePrice/src/edu/nju/comparePrice/actions/CandidateWordAction.java
@@ -2,6 +2,7 @@ package edu.nju.comparePrice.actions;
 
 import java.util.ArrayList;
 
+import edu.nju.comparePrice.models.Brand;
 import edu.nju.comparePrice.models.CandidateWord;
 import edu.nju.comparePrice.models.SpecialWord;
 import edu.nju.comparePrice.services.CommentService;
@@ -11,7 +12,11 @@ public class CandidateWordAction extends BaseAction{
 
 	private ArrayList<SpecialWord> candidateWordList;
 	
+	private String candidateWord;
+	private String brandId;
+	
 	private CommentService commentService;
+	private BrandService brandService;
 	
 	@Override
 	public String execute () {
@@ -22,6 +27,16 @@ public class CandidateWordAction extends BaseAction{
 	}
 	
 	public String add() {
+		Brand brand = fetchBrand(brandId);
+		if (brand == null) {
+			return ERROR;
+		}
+		
+		SpecialWord specialWord = new SpecialWord();
+		specialWord.setBid(brand.getId());
+		specialWord.setBrand(brand);
+		specialWord.setName(candidateWord);
+		commentService.addSpecialWord(specialWord);
 		return SUCCESS;
 	}
 
@@ -32,5 +47,33 @@ public class CandidateWordAction extends BaseAction{
 	public void setCommentService(CommentService commentService) {
 		this.commentService = commentService;
 	}
+
+	public void setCandidateWord(String candidateWord) {
+		this.candidateWord = candidateWord;
+	}
+
+	public void setBrandId(String brandId) {
+		this.brandId = brandId;
+	}
+	
+	private Brand fetchBrand (String brandIdString) {
+		int brandId = 0;
+		try{
+			brandId = Integer.parseInt(brandIdString);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+		
+		boolean isValid = brandService.contains(brandId);
+		boolean isValid = true;
+		if (isValid) {
+			Brand brand = new Brand();
+			brand.setId(brandId);
+			return brand;
+		} else {
+			return null;
+		}
+	}
+	
 	
 }
